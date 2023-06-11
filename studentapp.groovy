bb97a87878380775a7967ef6369d097888c41c09
smit <smitkotkar42@gmail.com>
2023-06-11 13:13:16 +0530

    ljbhb

---
+++ studentapp.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+    stages {
+        stage ('pull') {
+            steps {
+                git branch: 'main', url: 'https://github.com/smitkotkar/studentapp-ui.git'
+            }
+        }
+        stage ('build') {
+            steps {
+                echo 'Congrates! build is done'
+            }
+        }
+        stage ('test') {
+            steps {
+                echo 'test is succeed'
+            }
+        }
+        stage ('deploy') {
+            steps {
+                echo 'deploy is succeed'
+            }
+        }
+    }
+}
