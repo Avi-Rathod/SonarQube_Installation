908ebbb20495f270a4356abe41ed4ae24aec5b7a
smit <smitkotkar42@gmail.com>
2023-06-18 13:27:01 +0530

    jhjv

--- studentapp.groovy
+++ studentapp.groovy
@@ -16,16 +16,16 @@ pipeline {
                 sh '/opt/apache-maven/bin/mvn clean package'
             }
         }
-        // stage ('test') {
-        //     steps {
-        //         echo 'test is succeed'
-        //     }
-        // }
         stage ('test') {
             steps {
-                sh '/opt/apache-maven/bin/mvn sonar:sonar -Dsonar.projectKey=student-app -Dsonar.host.url=http://3.101.80.224:9000 -Dsonar.login=a3cc1415671986cd696512f8b285bf5949018ee5'
+                echo 'test is succeed'
             }
         }
+        // stage ('test') {
+        //     steps {
+        //         sh '/opt/apache-maven/bin/mvn sonar:sonar -Dsonar.projectKey=student-app -Dsonar.host.url=http://3.101.80.224:9000 -Dsonar.login=a3cc1415671986cd696512f8b285bf5949018ee5'
+        //     }
+        // }
         // stage ('build-test') {
         //     steps {
         //         withSonarQubeEnv(installationName:'sonar-server',credentialsId: 'sonar-token') { 
