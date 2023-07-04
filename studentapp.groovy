437e486b196e11c7169998ae910a747aa6d51024
smit <smitkotkar42@gmail.com>
2023-07-04 18:53:13 +0530

    quality gate

--- studentapp.groovy
+++ studentapp.groovy
@@ -16,11 +16,18 @@ pipeline {
         //         sh '/opt/apache-maven/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp -Dsonar.host.url=http://18.222.206.143:9000  -Dsonar.login=dc087b4b903d9453581131fcb788e16f380def34'
         //     }
         // }
-        stage('test') {
+        stage('build-test') {
             steps {
                  withSonarQubeEnv(installationName:'sonar-server',credentialsId: 'sonar_token') { 
                    sh '/opt/apache-maven/bin/mvn sonar:sonar -Dsonar.ProjectKey=studentapp'
                }
+            }
+        }
+        stage('quality-gate') {
+            steps {
+                timeout (time:10, unit:'MINUTES') {
+                    waitforQualityGate abortPipeline:true
+                }
             }
         }
         stage('deploy') {
