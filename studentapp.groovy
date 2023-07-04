4950843613b8fadd7d02243dc9ef00a5139c7ef1
smit <smitkotkar42@gmail.com>
2023-07-04 18:26:33 +0530

    sdgfs

--- studentapp.groovy
+++ studentapp.groovy
@@ -6,23 +6,23 @@ pipeline {
                 git 'https://github.com/shubhamkalsait/studentapp-ui.git'
             }
         }
-        // stage('build') {
-        //     steps {
-        //         sh '/opt/apache-maven/bin/mvn clean package'
-        //     }
-        // }
-        // stage('test') {
-        //     steps {
-        //         sh '/opt/apache-maven/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp -Dsonar.host.url=http://18.222.206.143:9000  -Dsonar.login=dc087b4b903d9453581131fcb788e16f380def34'
-        //     }
-        // }
-        stage('build-test') {
+        stage('build') {
             steps {
-                 withSonarQubeEnv(installationName:'sonar-server',credentialsId: 'sonar-token') { 
-                   sh '/opt/apache-maven/bin/mvn sonar:sonar -Dsonar.ProjectKey=studentapp'
-               }
+                sh '/opt/apache-maven/bin/mvn clean package'
             }
         }
+        stage('test') {
+            steps {
+                sh '/opt/apache-maven/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp -Dsonar.host.url=http://18.222.206.143:9000  -Dsonar.login=dc087b4b903d9453581131fcb788e16f380def34'
+            }
+        }
+        // stage('build-test') {
+        //     steps {
+        //          withSonarQubeEnv(installationName:'sonar-server',credentialsId: 'sonar-token') { 
+        //            sh '/opt/apache-maven/bin/mvn sonar:sonar -Dsonar.ProjectKey=studentapp'
+        //        }
+        //     }
+        // }
         stage('deploy') {
             steps {
                 echo 'deploy is done'
