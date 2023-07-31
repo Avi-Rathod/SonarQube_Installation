dcff9a5fc763d8d9d7df74c56cff701be0210b7b
smit <smitkotkar42@gmail.com>
2023-07-31 19:03:14 +0530

    sfdg

---
+++ studentapp-ui-31july.groovy
@@ -0,0 +1,25 @@
+Pipeline {
+    agent any
+    stages {
+        stage ('Pull') {
+            steps {
+                git branch: 'main', url: 'https://github.com/smitkotkar/studentapp-ui.git'
+            }
+        }
+        stage ('Build') {
+            steps {
+                echo 'build is succeed'
+            }
+        }
+        stage ('Test') {
+            steps {
+                echo 'Test is done'
+            }
+        }
+        stage ('Deploy') {
+            steps {
+                echo 'Deployment is done'
+            }
+        }
+    }
+}
