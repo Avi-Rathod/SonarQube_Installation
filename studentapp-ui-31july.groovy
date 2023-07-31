880ef92d81ed40fea1dbd0ec1ad5c9739176ca3b
smit <smitkotkar42@gmail.com>
2023-07-31 19:10:27 +0530

    url

--- studentapp-ui-31july.groovy
+++ studentapp-ui-31july.groovy
@@ -3,7 +3,7 @@ Pipeline {
     stages {
         stage ('Pull') {
             steps {
-                git branch: 'main', url: 'https://github.com/shubhamkalsait/studentapp-ui.git'
+                git 'https://github.com/shubhamkalsait/studentapp-ui.git'
             }
         }
         stage ('Build') {
